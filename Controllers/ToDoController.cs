140721c49330d67387614a4e8db71a9bd1408693
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-04-04 18:49:53 -0400

    ToDo page added with Home and Privacy. Made some changes in ToDoController class

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Data;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoDatabase _database;
+
+        public ToDoController(ToDoDatabase db)
+        {
+            _database = db;
+        }
+
+        public async Task<IActionResult> Index()
+        { 
+            var todos = await _database.ToDos.ToListAsync();
+            return View(todos);
+        }
+
+        public async Task<IActionResult> Add(ToDoItem item) {
+            _database.ToDos.Add(item);
+            await _database.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Complete(int id)
+        {
+            var todo = await _database.ToDos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            todo.IsComplete = true;
+            await _database.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Data/ToDoDatabase.cs
+++ Data/ToDoDatabase.cs
@@ -1,6 +1,26 @@
-﻿namespace ToDoApp.Data
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data
 {
-    public class ToDoDatabase
+    public class ToDoDatabase : DbContext
     {
+        public string DbPath { get; }
+
+        public ToDoDatabase(DbContextOptions<ToDoDatabase> options) : base (options)
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+        public DbSet<ToDoItem> ToDos { get; set; }
+
     }
+
+    
 }
